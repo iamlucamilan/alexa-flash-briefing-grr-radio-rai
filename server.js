a2d1b2b689c836c9567944439e2ed67bda5dc9ea
Luca Milan <milan.luca@gmail.com>
2018-01-12 16:36:35 +0100

    Add support for newrelic heroku addon

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const newrelic = require('newrelic');
 const express = require('express');
 const moment = require('moment-timezone');
 const fetch = require('node-fetch');
@@ -13,10 +14,10 @@ function setTime(time) {
   var utc = rome.clone().tz("Europe/London");
   return utc.format();
 };
-       
+
 
 // `res.json` converts JavaScript objects to JSON and
-// appropriately sets the Content-Type header to 
+// appropriately sets the Content-Type header to
 // application/json; charset=utf-8 . By default,
 // we'll get a 200 HTTP status code.
 app.get('/', (req, res) => {
@@ -27,17 +28,17 @@ app.get('/', (req, res) => {
   .then( json => {
    //console.log(json);
     const relevant = json.items.slice(json.items.length - 1);
-    
+
     console.log(relevant);
-    
+
     const uriFetches = relevant
     .map(r => r.mediauri)
     .map(mediauri => fetch(mediauri));
-    
+
     return Promise.all(uriFetches).then(values => {
       const urls = values.map(v => v.url);
       console.log(urls);
-      
+
       res.json(
         relevant.map((r, idx) => ({
           uid: r.uniquename,
@@ -46,7 +47,7 @@ app.get('/', (req, res) => {
           "mainText": "",
           streamUrl: urls[idx].replace('http://', 'https://'),
           redirectionUrl: "https://grr.rai.it",
-          
+
         }))
       );
     })
@@ -55,6 +56,6 @@ app.get('/', (req, res) => {
     console.log(err);
   });
 });
-      
+
 app.listen(process.env.PORT || 8080, () => console.log(
   `Your app is listening on port ${process.env.PORT || 8080}`));
